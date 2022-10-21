88f1c35d6501d7d88f8e08d6baef0e02a96ca1fe
digitensions <pittschick@me.com>
2022-10-21 09:19:40 +0000

    Add file duration to info.csv

--- code/get_stream_info.py
+++ code/get_stream_info.py
@@ -115,7 +115,7 @@ def get_metadata(filepath):
         return running_list
 
 
-def configure_data(metadata, channel):
+def configure_data(metadata, channel, actual_duration):
     '''
     Cut up metadata string and format for CSV write
     '''
@@ -137,7 +137,9 @@ def configure_data(metadata, channel):
         title = title.replace('\x87', '')
         desc = data[1].split('en:')[-1].strip()
         duration = data[-2].strip()
-        return [chnl, title, desc, date, time, duration, duration]
+        if not actual_duration:
+            actual_duration = duration
+        return [chnl, title, desc, date, time, duration, actual_duration]
     else:
         return None
 
@@ -153,6 +155,24 @@ def check_times(mdata, dt_start):
     if str(start) not in str(mdata):
         return None
     return mdata
+
+
+def get_duration(fpath):
+    '''
+    Mediainfo retrieval of duration and reform to HH:MM:SS
+    '''
+
+    cmd = [
+        'mediainfo', '--Language=raw',
+        '--Full', '--Inform="General;%Duration/String3%"',
+        fpath
+    ]
+    cmd[3] = cmd[3].replace('"', '')
+    duration = subprocess.check_output(cmd)
+    duration = duration.decode('utf-8')
+    print(duration)
+    if len(duration) > 8:
+        return str(duration)[:8]
 
 
 def main():
@@ -176,6 +196,7 @@ def main():
 
         for folder in s_folders:
             running_data = []
+            actual_duration = ''
             fpath = os.path.join(spath, folder)
             dt_end, dt_start, duration = get_end_time(folder, TODAY_DATE)
             now = datetime.utcnow()
@@ -189,6 +210,7 @@ def main():
                     LOGGER.info("Broadcast end: %s", datetime.strftime(dt_end, FORMAT))
                     LOGGER.info("Passed end time for broadcast, checking for metadata 'Running' data")
                     streampath = os.path.join(fpath, 'stream.mpeg2.ts')
+                    actual_duration = get_duration(streampath)
                     running_data = get_metadata(streampath)
                     if not running_data:
                         LOGGER.warning("No 'Running' metadata found in this folderpath: %s", streampath)
@@ -210,12 +232,12 @@ def main():
 
             if not match:
                 continue
-            match_data = configure_data(match, chnl)
+            match_data = configure_data(match, chnl, actual_duration)
             LOGGER.info("Matched data found: %s", match_data)
             # Create info.csv and write data to it
             csv_path = os.path.join(fpath, 'info.csv')
             LOGGER.info("Writing data to: %s", csv_path)
-            write_to_csv(csv_path, match_data)
+            write_to_csv(csv_path, match_data, actual_duration)
 
         try:
             y_folders = [x for x in os.listdir(ypath) if os.path.isdir(os.path.join(ypath, x))]
@@ -258,7 +280,7 @@ def main():
 
             if not match:
                 continue
-            match_data = configure_data(match, chnl)
+            match_data = configure_data(match, chnl, actual_duration)
             LOGGER.info("Matched data found: %s", match_data)
             # Create info.csv and write data to it
             csv_path = os.path.join(fpath, 'info.csv')
